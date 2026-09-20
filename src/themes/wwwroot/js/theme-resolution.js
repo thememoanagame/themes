39028f961d631935d5fd0dff43c03aa26526460f
Weslley Luiz <37993565+weslleymurdock@users.noreply.github.com>
2026-09-19 22:22:03 -0300

    Accept canonical GUID and UUIDv7 theme identifiers

--- src/themes/wwwroot/js/theme-resolution.js
+++ src/themes/wwwroot/js/theme-resolution.js
@@ -26,7 +26,7 @@
         return;
     }
 
-    if (id && !/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(id)) {
+    if (id && !/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(id)) {
         redirectError("invalid-query.json");
         return;
     }
